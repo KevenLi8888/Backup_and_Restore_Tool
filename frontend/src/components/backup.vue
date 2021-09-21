3e05507a919a688756a45d3482745cc4a6c09f3e
kevenli <keven0826@icloud.com>
2021-09-21 09:15:42 +0800

    building frontend

--- frontend/src/components/backup.vue
+++ frontend/src/components/backup.vue
@@ -6,9 +6,12 @@
           <v-card-title>
             Select source
           </v-card-title>
+          <v-card-text>
+            {{srcPath}}
+          </v-card-text>
           <v-card-actions>
             <v-layout justify-center align-center class="px-0">
-              <v-btn color="#81D4FA" @click="selectDir">Open...</v-btn>
+              <v-btn color="#81D4FA" @click="selectSrcDir">Open...</v-btn>
             </v-layout>
           </v-card-actions>
         </v-card>
@@ -34,14 +37,42 @@
           <v-card-title>
             Select destination
           </v-card-title>
+          <v-card-text>
+            {{destPath}}
+          </v-card-text>
           <v-card-actions>
             <v-layout justify-center align-center class="px-0">
-              <v-btn color="blue" @click="getMessage">Press Me!!!</v-btn>
+              <v-btn color="#81D4FA" @click="selectDestDir">Open...</v-btn>
             </v-layout>
           </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col cols="12" sm="6" offset-sm="3">
+        <v-layout justify-center align-center class="px-0">
+          <v-btn fab color="success" disabled="!activateButton">
+            <v-icon>
+              backup
+            </v-icon>
+          </v-btn>
+        </v-layout>
+      </v-col>
+    </v-row>
+
+    <div class='text-xs-center'>
+      <v-dialog v-model="errorDialog" width="500">
+        <v-card>
+          <v-card-title class="headline" primary-title>ERROR!</v-card-title>
+          <v-card-text>{{errorMessage}}</v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="errorDialog = false">Fine</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
     <div class="text-xs-center">
       <v-dialog v-model="dialog" width="500">
         <v-card>
@@ -51,6 +82,7 @@
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="primary" text @click="dialog = false">Awesome</v-btn>
+<!--            TODO:待优化，只保留一个考虑message是否要清空 -->
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -62,15 +94,75 @@
   export default {
     data () {
       return {
-        message: " ",
+        message: "",
         raised: true,
-        dialog: false
+        dialog: false,
+        errorDialog: false,
+        errorMessage: " ",
+        srcPath: "",
+        destPath: "",
+        activateButton: false
+      }
+    },
+
+    computed: {
+      listenChange() {
+        const srcPath = this.srcPath
+        const destPath = this.destPath
+        return {srcPath, destPath}
+      }
+    },
+
+    watch: {
+      listenChange:{
+        handler:function(val) {
+          console.log('listening change' + val.srcPath + val.destPath)
+          var self = this
+          if (val.srcPath != "" && val.destPath != "") {
+            self.activateButton = true
+        }
+      },
+        deep: true
       }
     },
 
     methods: {
-      selectDir: function () {
-        window.backend.Backup.SelectSourceDir();
+      selectSrcDir: function () {
+        var self = this
+        window.backend.Backup.SelectSourceDir()
+            .then(result => {
+              var self = this
+              self.srcPath = result
+            })
+            .catch(error => {
+              if (self.srcPath == "") {
+                self.errorMessage = error
+                self.errorDialog = true
+                setTimeout(() => {
+                  self.errorMessage = ""
+                  self.errorDialog = false
+                }, 5000)
+              }
+            });
+      },
+
+      selectDestDir: function () {
+        var self = this
+        window.backend.Backup.SelectDestDir()
+            .then(result => {
+              var self = this
+              self.destPath = result
+            })
+            .catch(error => {
+              if (self.destPath == "") {
+                self.errorMessage = error
+                self.errorDialog = true
+                setTimeout(() => {
+                  self.errorMessage = ""
+                  self.errorDialog = false
+                }, 5000)
+              }
+            });
       },
 
       getMessage: function () {
@@ -115,4 +207,6 @@
     padding: 9px;
     cursor: pointer;
   }
+
+
 </style>
--- ui/backup.go
+++ ui/backup.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"fmt"
 	"github.com/wailsapp/wails"
 )
 
